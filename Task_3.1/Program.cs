b021bef1ec0f776e995a55c465fcb8c3ea763c13
Yulia <rosiely@mail.ru>
2022-08-10 22:19:31 +0500

    Задачи заданные в самом семинаре

---
+++ Task_3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿//либо решение через string
+
+Console.Write("Введи число: ");
+
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+
+string anyNumberText = Convert.ToString(anyNumber);
+
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine($"третья цифра числа {anyNumber} равна {anyNumberText[2]}");
+}
+else 
+{
+  Console.WriteLine("третьей цифры нет");
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+while (a > 999)
+{
+a = a / 10;
+}
+if (a < 100) 
+{
+Console.WriteLine("третьей цифры нет");
+}
+else Console.WriteLine($"третья цифра числа {a} равна {a % 10}");
+
+//либо решение через string
+//Console.Write("Введи число: ");
+//int anyNumber = Convert.ToInt32(Console.ReadLine());
+//string anyNumberText = Convert.ToString(anyNumber);
+//if (anyNumberText.Length > 2){
+//  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+//}
+//else {
+//  Console.WriteLine("-> третьей цифры нет");
+//}
